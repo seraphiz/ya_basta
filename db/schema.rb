43df63ee9ca9c4c03cc2d491d89d360e3f5a82c4
Cristian <cristian.arthur@hotmail.com>
2019-03-03 14:00:14 -0300

    Commit 2 - Tabla Post arreglada, nav bar agragado no finalizado

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_161720) do
+ActiveRecord::Schema.define(version: 2019_03_03_165742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.string "content"
     t.date "date"
     t.string "period_of_day"
