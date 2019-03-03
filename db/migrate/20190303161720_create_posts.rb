10410d7ce8cefb2f66c0736ff68280d0e7f8d612
Cristian <cristian.arthur@hotmail.com>
2019-03-03 13:24:52 -0300

    Inicializando proyecto. Devise/Cancanca gemas agregadas - Devise instalado - Controller y Model Post - Commit 1

---
+++ db/migrate/20190303161720_create_posts.rb
@@ -0,0 +1,18 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.date :date
+      t.string :period_of_day
+      t.integer :age
+      t.boolean :anonymous, default: false
+      t.string :adress
+      t.float :latitude
+      t.float :longitude
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
